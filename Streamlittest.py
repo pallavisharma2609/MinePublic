f2ab02653c2038601c8da9f61aab0fd32b4a4278
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-08-28 13:26:31 +0530

    Update Streamlittest.py

--- Streamlittest.py
+++ Streamlittest.py
@@ -3,11 +3,35 @@
 Here's our first attempt at using data to create a table:
 """
 
+
 import streamlit as st
+import snowflake.connector
 import pandas as pd
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
 df
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from trips;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
