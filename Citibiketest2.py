dc9ce601f6316fe01616df6713c79b8c76e20597
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-08-28 15:19:26 +0530

    Update Citibiketest2.py

--- Citibiketest2.py
+++ Citibiketest2.py
@@ -8,8 +8,9 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
+import folium
+from streamlit_folium import folium_static
+import base64
 
 
 # Initialize connection.
