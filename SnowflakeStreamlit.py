6ec1ff26cc96ec2a23de958df25d1683f5470811
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-02 19:40:18 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -116,15 +116,15 @@ st.bokeh_chart(graph2, use_container_width=True)
 
 output_file("colormapped_bars.html")
 
-fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
-counts = [5, 3, 4, 2, 4, 6]
+#fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+#counts = [5, 3, 4, 2, 4, 6]
 
-source = ColumnDataSource(data=dict(fruits=fruits, counts=counts, color=Spectral6))
+source = ColumnDataSource(data=dict(fruits=months, counts=numberofbikes, color=Spectral6))
 
-p = figure(x_range=fruits, y_range=(0,9), height=250, title="Fruit counts",
+p = figure(x_range=months, y_range=numberofbikes, height=250, title="Fruit counts",
            toolbar_location=None, tools="")
 
-p.vbar(x='fruits', top='counts', width=0.9, color='color', legend_field="fruits", source=source)
+p.vbar(x=months, top=numberofbikes, width=0.9, color='color', legend_field="months", source=source)
 
 p.xgrid.grid_line_color = None
 p.legend.orientation = "horizontal"
