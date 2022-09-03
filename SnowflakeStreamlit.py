4bf3b3bbbe82b0185f91442c1b820a8f70f76c63
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-03 19:36:34 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -121,5 +121,5 @@ st.markdown(f'<h1 style="color:#ffd700;font-size:18px;">{"Number of Bikes per Mo
 st.bokeh_chart(graph2, use_container_width=True)
 st.snow()
 st.balloons()
-Function signature
+
 
