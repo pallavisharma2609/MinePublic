78852881b31b7e36a3bcb3116d604dd2699503d9
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-05 17:09:27 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -94,9 +94,9 @@ st.bokeh_chart(row(graph, graph1))
 st.snow()
 #fig = make_subplots(rows=1, cols=2)
 
-fig1 = px.bar(df2, x='MONTH', y='NUMBER_OF_TRIPS',
+fig1 = px.bar(df2, x='MONTH', y='NUMBER_OF_TRIPS',\
              title = "Number of Trips per Months",width=450, height=350,row=1, col=1)
-fig2 = px.bar(df2, x='MONTH', y='NUMBER_OF_BIKES',
+fig2 = px.bar(df2, x='MONTH', y='NUMBER_OF_BIKES',\
              title = "Number of Bikes per Months",width=450, height=350,row=1, col=2)
 
 st.plotly_chart(fig1)
