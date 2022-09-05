4de61324352baabb502e6c957345a098c2dda070
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-05 18:50:25 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -43,8 +43,8 @@ select_month_slider = st.sidebar.select_slider('Use slider to display Month rang
 st.write('I have selected Months till ', select_month_slider, ' of Year ',year_choice)
 numberoftrips = df1['NUMBER_OF_TRIPS'].loc[df1["YEAR"] == year_choice].loc[df1["MONTH"] <= select_month_slider]
 numberofbikes = df1['NUMBER_OF_BIKES'].loc[df1["YEAR"] == year_choice].loc[df1["MONTH"] <= select_month_slider]
-countfemale = df1['COUNT_FEMALE'].loc[df1["YEAR"] == year_choice].loc[df1["MONTH"] == select_month_slider]
-countmale = df1['COUNT_MALE'].loc[df1["YEAR"] == year_choice].loc[df1["MONTH"] == select_month_slider]
+countfemale = df1['COUNT_FEMALE'].loc[df1["YEAR"] == year_choice].loc[df1["MONTH"] <= select_month_slider]
+countmale = df1['COUNT_MALE'].loc[df1["YEAR"] == year_choice].loc[df1["MONTH"] <= select_month_slider]
 
 
 
