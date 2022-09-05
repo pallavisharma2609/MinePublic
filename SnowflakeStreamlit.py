a3226c559bfa7e47e90ddc41f5ba28e78b90b1b8
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-05 17:57:05 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -67,7 +67,7 @@ bind_socket()
 #fig = make_subplots(rows=1, cols=2)
 
 
-fig = make_subplots(rows=1, cols=2,subplot_titles=("Number of Trips per Month", "Number of Bikes per Month"),column_widths =[0.5,0.5])
+fig = make_subplots(rows=1, cols=2,subplot_titles=("Number of Trips per Month", "Number of Bikes per Month"),column_widths =[450,450],vertical_spacing=0.1)
 fig.add_trace(
     go.Bar( x=months, y=numberoftrips),row=1, col=1)
 #Second SubPlot
