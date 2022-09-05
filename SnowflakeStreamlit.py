67adc400e11d350cdd5e7f43e8b95e9a669f5318
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-05 17:35:17 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -44,10 +44,7 @@ years = df1["YEAR"].drop_duplicates()
 year_choice = st.sidebar.selectbox('Select Year', years) 
 months = df1["MONTH"].loc[df1["YEAR"] == year_choice]
 select_month_range = sorted(months.unique())
-st.write(years)
-st.write(year_choice)
-st.write(months)
-st.write(select_month_range)
+
 
 
 select_month_slider = st.sidebar.select_slider('Use slider to display Month range:',select_month_range)
@@ -60,7 +57,7 @@ countmale = df1['COUNT_MALE'].loc[df1["YEAR"] == year_choice].loc[df1["MONTH"] =
 
 
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def bind_socket():
     # This function will only be run the first time it's called
     st.snow()
