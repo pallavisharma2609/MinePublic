d9d280c6a77654acd6cfdf056834dea75bf50645
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-05 12:30:34 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -107,6 +107,6 @@ st.bokeh_chart(row(graph, graph1))
 st.snow()
 
 fig = px.bar(df1, x='MONTH', y='NUMBER_OF_TRIPS',
-            color='NUMBER_OF_TRIPS',
+            color='MONTH',
             height=400)
 st.plotly_chart(fig)
