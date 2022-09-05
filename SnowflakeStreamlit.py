f0999c54f76a3fcb18eae87b0a65efb7bc58494e
pallavisharma2609 <112246844+pallavisharma2609@users.noreply.github.com>
2022-09-05 12:20:04 +0530

    Update SnowflakeStreamlit.py

--- SnowflakeStreamlit.py
+++ SnowflakeStreamlit.py
@@ -105,4 +105,4 @@ df2 = [months,numberoftrips]
 
 st.snow()
 #st.plotly_chart(fig, use_container_width=True)
-st.bar_chart( df2,x=months, y=numberoftrips, width=0, height=0, use_container_width=True)
+#st.bar_chart( df2,x=months, y=numberoftrips, width=0, height=0, use_container_width=True)
